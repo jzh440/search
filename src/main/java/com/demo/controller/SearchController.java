39f1295a1261f9f7313d46071d6758115a9fe4a4
jzh <jingzh@163.com>
2017-02-23 18:25:24 +0800

    aaa

--- src/main/java/com/demo/controller/SearchController.java
+++ src/main/java/com/demo/controller/SearchController.java
@@ -1,7 +1,13 @@
 package com.demo.controller;
 
+import com.demo.model.Waps;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RequestMapping("search")
 @Controller
@@ -12,4 +18,19 @@ public class SearchController {
 		
 		return "search/index";
 	}
+
+	/**
+	 * 查询统计指标
+	 *
+	 * @return
+	 */
+	@RequestMapping(value = "query", method = RequestMethod.GET, produces = "application/json")
+	public List<Waps> queryIndexs(@RequestParam("keywords") String keywords) {
+		List<Waps> wapses = new ArrayList<>();
+		wapses.add(new Waps());
+		wapses.add(new Waps());
+		wapses.add(new Waps());
+		return wapses;
+	}
+
 }
