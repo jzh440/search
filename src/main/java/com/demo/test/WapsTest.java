53a5b1a8dd6145ce1a56ee545eb6140ffd703a11
jzh <jingzh@163.com>
2017-02-23 18:22:21 +0800

    aaa

--- src/main/java/com/demo/test/WapsTest.java
+++ src/main/java/com/demo/test/WapsTest.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import javax.annotation.Resource;
 
+import com.demo.dao.WapsMapper;
+import com.demo.model.Waps;
 import org.junit.Test;
 
 
@@ -16,6 +18,10 @@ public class WapsTest extends SpringJunitTest {
 
     @Test
     public void testQuery(){
+        WapsMapper wapsMapper =  acx.getBean(WapsMapper.class);
+        Waps waps = new Waps();
+        waps.setWapKeyWord("haha");
+        wapsMapper.query(waps);
         /*KeysMapper mapper = acx.getBean(KeysMapper.class);
         Keys key = new Keys();
         key.setKeyWord("haha");
